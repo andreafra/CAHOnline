4ba95f13d3b5026464dda82642f15cedc6702999
YoDevil <alessandro.astone@hotmail.com>
2017-01-26 17:01:29 +0100

    Showing players in room
    Also handling when a player leaves a room
    Also fixed a lot of stuff

--- client.js
+++ client.js
@@ -17,23 +17,30 @@ app.config(function($routeProvider, $locationProvider) {
 app.controller("joinGame", function ($scope, $routeParams){
   $scope.room=$routeParams.room
   $scope.id=socket.io.engine.id
+
+  socket.on('display_players', function(data){
+    $scope.players=data.players
+    $scope.$apply()
+  })
+
+  socket.emit('fetch_players',{room: $routeParams.room})
+
 })
 
 app.controller('mainCtrl', function($scope, $location) {
-      console.log("LOL")
+  //if user clicked to go back to home we should remove him from the players, until he joins another room
+  socket.emit("delete_player")
 
   $scope.createRoom = function() {
-    if ($scope.playerName !== "" && $scope.playerName.length > 2) {
-      socket.emit("create_room", { 
-        name: $scope.playerName 
-      })
+    if ($scope.playerName && $scope.playerName.length > 2) {
+      socket.emit("create_room", {name: $scope.playerName})
     } else {
       alert("Name is too short or void")
     }
   }
 
   $scope.joinRoom = function() {
-    if ($scope.roomId !== "" && $scope.playerName !== "" && $scope.playerName.length > 2) {
+    if ($scope.roomId && ($scope.playerName ? $scope.playerName.length > 2 : false)) {
       socket.emit("join_room", { 
         name: $scope.playerName,
         roomId: $scope.roomId })
@@ -42,8 +49,8 @@ app.controller('mainCtrl', function($scope, $location) {
     }
   }
 
-  socket.on('load_game_page', function(roomId){
-    $location.path("/"+roomId)
+  socket.on('load_game_page', function(data){
+    $location.path("/"+data.room)
     $scope.$apply()
   })
 })
--- server.js
+++ server.js
@@ -24,8 +24,6 @@ io.on("connection", function(socket) {
   
   // create room and join it
   socket.on("create_room", function(data) {
-    console.log("Name: " + data.name + " Id: " + socket.id) //DEBUG
-
     var roomId = shortid.generate()
     data.roomId = roomId;
     socket.join(roomId)
@@ -40,11 +38,28 @@ io.on("connection", function(socket) {
   })
 
   socket.on('disconnect', function() {
-  	delete players[socket.id]
+  	deletePlayer(socket.id)
   	console.log("Player " + socket.id + " disconnected");
+  })
+
+  socket.on('delete_player', function() {
+  	deletePlayer(socket.id)
+  })
+
+  socket.on('fetch_players', function(data){
+	var playersInSameRoom = getPlayersInRoom(data.room)
+	io.to(data.room).emit("display_players", {players: playersInSameRoom})
   })
 })
 
+function getPlayersInRoom(room){
+	var socketsInSameRoom = io.nsps['/'].adapter.rooms[room] ? io.nsps['/'].adapter.rooms[room].sockets : null
+	var playersInSameRoom = {}
+	for(var otherId in socketsInSameRoom){
+	  playersInSameRoom[otherId]=players[otherId]
+	}
+	return playersInSameRoom
+}
 
 function addPlayer(player) {
   players[player.socket.id] = {
@@ -53,6 +68,16 @@ function addPlayer(player) {
     playerRoom: player.data.roomId,
     points: 0
   }
-  player.socket.emit("load_game_page", player.data.roomId)
+
+  player.socket.emit("load_game_page", {room: player.data.roomId})
   //console.log("Players : " + JSON.stringify(players))
+}
+
+function deletePlayer(id) {
+	if(players[id]){
+		var playerRoom = players[id].playerRoom
+		delete players[id]
+		var playersInSameRoom = getPlayersInRoom(playerRoom)
+		io.to(playerRoom).emit("display_players", {players: playersInSameRoom})
+	}
 }
