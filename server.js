dd5abc99da0cdee9285d73c6deb0c62cb33b0563
YoDevil <alessandro.astone@hotmail.com>
2017-01-29 18:57:07 +0100

    Fixed crash if reloading when alone in lobby

--- server.js
+++ server.js
@@ -62,7 +62,8 @@ io.on("connection", function(socket) {
 
   socket.on('get_first_load', function(data){
     var playersInSameRoom = getPlayersInRoom(data.room)
-    if(playersInSameRoom.length == 0){
+    if(!io.nsps['/'].adapter.rooms[data.room]){
+      socket.join(data.room)
       io.nsps['/'].adapter.rooms[data.room].gameState = 0
     }
     socket.emit('first_load',{players: playersInSameRoom, gameState: io.nsps['/'].adapter.rooms[data.room].gameState})
