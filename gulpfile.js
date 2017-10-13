fe075797260c42c413711ee4817d64eb578725d8
Andrea Franchini <f.andrea1602@gmail.com>
2017-10-13 16:15:01 +0200

    Reworked UI

--- gulpfile.js
+++ gulpfile.js
@@ -4,8 +4,9 @@
 // setup is now using Gulp. It works exactly how I expect it to and is WAY more concise.
 var gulp = require('gulp'),
     spawn = require('child_process').spawn,
+    sass = require('gulp-sass'),
+    browserSync = require('browser-sync').create(),
     node;
-
 /**
  * $ gulp server
  * description: launch the server. If there's a server already running, kill it.
@@ -15,28 +16,49 @@ gulp.task('server', function() {
   node = spawn('node', ['server.js'], {stdio: 'inherit'})
   node.on('close', function (code) {
     if (code === 8) {
-      gulp.log('Error detected, waiting for changes...');
+      gulp.log('Error detected, waiting for changes...')
     }
   });
+})
+
+/**
+ * compile scss to scss
+ */
+gulp.task('sass', function() {
+  return gulp.src('app/scss/*.scss')
+    .pipe(sass())
+    .pipe(gulp.dest('app/css'))
+})
+
+/**
+ * LiveReload the page every css change
+ */
+gulp.task('browserSync', function() {
+  browserSync.init({
+    proxy: {
+      target: "localhost:5000"
+    }
+  })
+})
+
+gulp.task('reload', function() {
+  browserSync.reload();
+})
+
+gulp.task('watch', function() {
+  gulp.watch('server.js', ['server']);
+  gulp.watch('app/**/*.scss', ['sass', 'reload']);
+  gulp.watch('app/**/*.html', ['reload']);
+  gulp.watch('app/js/**/*.js', ['reload']);
 })
 
 /**
  * $ gulp
  * description: start the development environment
  */
-gulp.task('default', function() {
-  gulp.run('server')
-
-  gulp.watch(['./server.js', './app/**/*.js'], function() {
-    gulp.run('server')
-  })
-  
-  // Need to watch for sass changes too? Just add another watch call!
-  // no more messing around with grunt-concurrent or the like. Gulp is
-  // async by default.
-})
+gulp.task('default', ['server', 'sass', 'browserSync', 'watch']);
 
 // clean up if an error goes unhandled.
 process.on('exit', function() {
-    if (node) node.kill()
+    if (node) node.kill();
 })
